413dcc80b8321d99c1299d60d95073c7091f02fd
gonzalo404 <gonzalo.merc@gmail.com>
2023-08-02 22:12:50 -0400

    nada

---
+++ src/main/java/com/sprint7/mvc/Sprint7Application.java
@@ -0,0 +1,13 @@
+package com.sprint7.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprint7/mvc/Sprint7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint7.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
